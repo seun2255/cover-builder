c03e13d8fea5e70832bb5a61e03dd9f227c54973
seun2255 <seunemmanuel2255@gmail.com>
2024-12-24 23:21:33 +0100

    feat: added check to image set generator

--- src/scripts/generateData.js
+++ src/scripts/generateData.js
@@ -1,17 +1,14 @@
 import { readdirSync, writeFileSync } from "fs";
 const files = readdirSync("public/cards");
 
-const imageData = files.map((file) => ({
-  src: `/cards/${file}`,
-  name: file,
-}));
+const imageData = files
+  .filter((file) => /\.(png|jpe?g|gif|svg)$/.test(file))
+  .map((file) => ({
+    src: `/cards/${file}`,
+    name: file,
+  }));
 
-const jsonArray = JSON.stringify([
-  ...imageData,
-  ...imageData,
-  ...imageData,
-  ...imageData,
-]);
+const jsonArray = JSON.stringify(imageData);
 writeFileSync("src/cards.json", jsonArray);
 
 console.log("Files saved to cards.json");
